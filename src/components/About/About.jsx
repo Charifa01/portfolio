8b586fe18a7f1dd8bf7d44629ba4d130744ad814
charifa <bounagatcharifa01@gmail.com>
2025-04-16 23:56:23 +0100

    commit changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,15 +12,15 @@ const About = ({ data }) => {
       <div className="container">
         <SectionHeading title="WELCOME TO..." subTitle="Nice to meet you!" />
         <div className="row">
-						<div className="col-lg-6 pt-3">
-							<div className="rounded-3 border-class position-relative h-100 overflow-hidden">
-								<div className="box-linear-animation p-md-6 p-3 ">
-                <div className="d-flex align-items-center">
-										<svg xmlns="http://www.w3.org/2000/svg" width={24} height={28} viewBox="0 0 24 28" fill="none">
-											<path className="fill-primary-2" d="M0 22.667V4.66699C0 2.45786 1.79087 0.666992 4 0.666992H22.6667C23.4031 0.666992 24 1.26395 24 2.00033V26.0003C24 26.7367 23.4031 27.3337 22.6667 27.3337H4.66667C2.08933 27.3337 0 25.2443 0 22.667ZM21.3333 24.667V20.667H4.66667C3.56209 20.667 2.66667 21.5625 2.66667 22.667C2.66667 23.7715 3.56209 24.667 4.66667 24.667H21.3333ZM9.33333 3.33366H4C3.26363 3.33366 2.66667 3.93062 2.66667 4.66699V18.4494C3.27284 18.1614 3.95093 18.0003 4.66667 18.0003H21.3333V3.33366H18.6667V14.0003L14 11.3337L9.33333 14.0003V3.33366Z" fill="#62A92B" />
-										</svg>
-										<h2 className="mb-0 ms-2">About Me</h2>
-									</div>
+			<div className="col-lg-6 pt-3">
+				<div className="rounded-3 border-class position-relative h-100 overflow-hidden">
+				  <div className="box-linear-animation p-md-6 py-4 px-3 ">
+                    <div className="d-flex align-items-center mb-5">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width={30} height={30}>
+							<path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" fill="#6333ff"/>
+						</svg>
+						<h2 className="mb-0 ms-2">About Me</h2>
+				</div>
                   <p>
                   I am a passionate Full-Stack Developer who thrives on turning ideas into powerful web applications. With expertise in React, Next.js, Node.js, Laravel, and the MERN stack, I build scalable and high-performance solutions that blend functionality with modern design.
 
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -5,10 +5,10 @@ export default function Education() {
 		<>
 			 <div className="col-lg-6 pt-3">
 							<div className="rounded-3 border-class position-relative h-100 overflow-hidden">
-								<div className="box-linear-animation p-md-6 p-3 ">
+								<div className="box-linear-animation p-md-6 py-4 px-3 ">
 									<div className="d-flex align-items-center">
-										<svg xmlns="http://www.w3.org/2000/svg" width={24} height={28} viewBox="0 0 24 28" fill="none">
-											<path className="fill-primary-2" d="M0 22.667V4.66699C0 2.45786 1.79087 0.666992 4 0.666992H22.6667C23.4031 0.666992 24 1.26395 24 2.00033V26.0003C24 26.7367 23.4031 27.3337 22.6667 27.3337H4.66667C2.08933 27.3337 0 25.2443 0 22.667ZM21.3333 24.667V20.667H4.66667C3.56209 20.667 2.66667 21.5625 2.66667 22.667C2.66667 23.7715 3.56209 24.667 4.66667 24.667H21.3333ZM9.33333 3.33366H4C3.26363 3.33366 2.66667 3.93062 2.66667 4.66699V18.4494C3.27284 18.1614 3.95093 18.0003 4.66667 18.0003H21.3333V3.33366H18.6667V14.0003L14 11.3337L9.33333 14.0003V3.33366Z" fill="#62A92B" />
+										<svg  xmlns="http://www.w3.org/2000/svg" width={24} height={28} viewBox="0 0 24 28" fill="none">
+											<path d="M0 22.667V4.66699C0 2.45786 1.79087 0.666992 4 0.666992H22.6667C23.4031 0.666992 24 1.26395 24 2.00033V26.0003C24 26.7367 23.4031 27.3337 22.6667 27.3337H4.66667C2.08933 27.3337 0 25.2443 0 22.667ZM21.3333 24.667V20.667H4.66667C3.56209 20.667 2.66667 21.5625 2.66667 22.667C2.66667 23.7715 3.56209 24.667 4.66667 24.667H21.3333ZM9.33333 3.33366H4C3.26363 3.33366 2.66667 3.93062 2.66667 4.66699V18.4494C3.27284 18.1614 3.95093 18.0003 4.66667 18.0003H21.3333V3.33366H18.6667V14.0003L14 11.3337L9.33333 14.0003V3.33366Z" fill="#6333ff" />
 										</svg>
 										<h2 className="mb-0 ms-2">Education</h2>
 									</div>
@@ -16,37 +16,19 @@ export default function Education() {
 										<ul className="ps-3">
 											<li className="position-relative z-1 mb-3">
 												<div className="d-flex gap-2">
-													<p className="text-300 text-nowrap fw-regular mb-0">2020-2024:</p>
+													<p className="text-300 text-nowrap fw-regular mb-0 fs-5">2020 – 2022:</p>
 													<div>
-														<span className="text-primary-2">MIT</span>
-														<p className="text-dark">Bachelor’s Degree in Computer Science</p>
+														<span className="text-primary-2 fs-5">High School Diploma in Physical Sciences</span>
+														<p className="text-secondary pt-2 fs-5">centre mixte de formation professionnelle (OFPPT), Agadir</p>
 													</div>
 												</div>
 											</li>
 											<li className="position-relative z-1 mb-3">
-												<div className="d-flex gap-2">
-													<p className="text-300 text-nowrap fw-regular mb-0">2018-2019:</p>
+											<div className="d-flex gap-2">
+													<p className="text-300 text-nowrap fw-regular mb-0 fs-5">2018 – 2019:</p>
 													<div>
-														<span className="text-primary-2">Harvard University</span>
-														<p className="text-dark">Certification in React and Redux, Node.js Developer Course</p>
-													</div>
-												</div>
-											</li>
-											<li className="position-relative z-1 mb-3">
-												<div className="d-flex gap-2">
-													<p className="text-300 text-nowrap fw-regular mb-0">2015-2016:</p>
-													<div>
-														<span className="text-primary-2">Stanford University</span>
-														<p className="text-dark">Certification in Full Stack Web Development</p>
-													</div>
-												</div>
-											</li>
-											<li className="position-relative z-1 mb-3">
-												<div className="d-flex gap-2">
-													<p className="text-300 text-nowrap fw-regular mb-0">2013-2015:</p>
-													<div>
-														<span className="text-primary-2">University of Washington</span>
-														<p className="text-dark">Certification in React and Redux, Node.js Developer Course</p>
+														<span className="text-primary-2 fs-5">High School Diploma in Physical Sciences</span>
+														<p className="text-secondary pt-2 fs-5">Abdellah Ben Yassin High School, Inezgane</p>
 													</div>
 												</div>
 											</li>
